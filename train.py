077932cdc505b3126edee66dfbfded519afd0219
Santi Puch Giner <santipuch590@gmail.com>
2020-05-21 17:49:55 +0200

    Minor formatting changes

--- train.py
+++ train.py
@@ -1,15 +1,18 @@
 import copy
-import diagnostics
+import random
 from pathlib import Path
+
+import numpy as np
+from sklearn.model_selection import train_test_split, StratifiedKFold
+import torch
+
+import diagnostics
 from datasets import ChestXRayPneumoniaDataset, COVIDChestXRayDataset, NIHCX38Dataset
 from models import Resnet34
 from trainer import Trainer
-from sklearn.model_selection import train_test_split, StratifiedKFold
 
-
-import random
-import numpy as np
-import torch
+# Run diagnostics
+diagnostics.run()
 
 # Fix seed to improve reproducibility
 SEED = 6666
@@ -25,24 +28,20 @@ n_splits = 5
 
 # Pretrain with Chest XRay Pneumonia dataset (>5k images)
 pneumonia_classifier = Resnet34()
-dataset = ChestXRayPneumoniaDataset(Path('data/chest-xray-pneumonia'), size)
+dataset = ChestXRayPneumoniaDataset(Path("data/chest-xray-pneumonia"), size)
 dataset.build()
 # dataset = NIHCX38Dataset(Path('input/nih-cx38'), size, balance=True)
-train_idx, validation_idx = train_test_split(
-    list(range(len(dataset))),
-    test_size=0.2,
-    stratify=dataset.labels
-)
+train_idx, validation_idx = train_test_split(list(range(len(dataset))), test_size=0.2, stratify=dataset.labels)
 trainer = Trainer(pneumonia_classifier, dataset, batch_size, train_idx, validation_idx)
 trainer.run(max_epochs=2)
 
 # Fine tune with COVID-19 Chest XRay dataset (~120 images)
-dataset = COVIDChestXRayDataset(Path('data/covid_chestxray'), size)
-print('Executing a {}-fold cross validation'.format(n_splits))
+dataset = COVIDChestXRayDataset(Path("data/covid_chestxray"), size)
+print("Executing a {}-fold cross validation".format(n_splits))
 split = 1
 skf = StratifiedKFold(n_splits=n_splits)
 for train_idx, validation_idx in skf.split(dataset.df, dataset.labels):
-    print('===Split #{}==='.format(split))
+    print("===Split #{}===".format(split))
     # Start from the pneumonia classifier
     classifier = copy.deepcopy(pneumonia_classifier)
     trainer = Trainer(classifier, dataset, batch_size, train_idx, validation_idx)
